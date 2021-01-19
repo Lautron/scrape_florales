bdedc8795a7686a428e8b2b1d9eeff2771051ad8
Lautaro Bachmann <laurigbach@gmail.com>
2021-01-19 19:12:52 -0300

    Temporarily reduced quantity of scraped links for testing purposes and changed content selector

--- scrape_florales/scrape_florales/spiders/florales.py
+++ scrape_florales/scrape_florales/spiders/florales.py
@@ -5,12 +5,14 @@ class FloralesSpider(scrapy.Spider):
     start_urls = ['https://web.archive.org/web/20200922142824/http://www.floralesmisioneras.com/']
 
     def parse(self, response):
-        links = response.css('#imPageExtContainer a::attr(href)')
+        #links = response.css('#imPageExtContainer a::attr(href)')
+        links = response.css('#imHeader_imMenuObject_04_container > ul > li:nth-child(3) > div.multiple-column > ul > li:nth-child(4) > ul > li:nth-child(2) > ul > li:nth-child(2) > ul a::attr(href)')
         yield from response.follow_all(links, self.parse_item)
 
     def parse_item(self, response):
         title = response.css('#imPgTitle::text').get()
         yield {
                 'title': title if title else response.css('title::text').get(),
-                'content': '\n'.join([res.get() for res in response.css('#imContent *:not(#imPgTitle)') if not res.get().startswith('<script>')]),
+                'content': '\n'.join(response.css('#imContent > *:not(script):not(header):not(a)').getall())
         }
+
