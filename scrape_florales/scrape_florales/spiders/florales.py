4ce60f6db9be54b035afa086f26c76cbdeaa3a0a
Lautaro Bachmann <laurigbach@gmail.com>
2021-01-21 21:03:03 -0300

    Added option to allow to choose the starting link from the CLI

--- scrape_florales/scrape_florales/spiders/florales.py
+++ scrape_florales/scrape_florales/spiders/florales.py
@@ -2,11 +2,13 @@ import scrapy
 
 class FloralesSpider(scrapy.Spider):
     name = 'florales'
-    start_urls = ['https://web.archive.org/web/20200922142824/http://www.floralesmisioneras.com/']
+    #start_urls = ['https://web.archive.org/web/20200922142824/http://www.floralesmisioneras.com/']
+    def __init__(self, *args, **kwargs): 
+          super(FloralesSpider, self).__init__(*args, **kwargs) 
 
+          self.start_urls = [kwargs.get('url')] 
     def parse(self, response):
-        #links = response.css('#imPageExtContainer a::attr(href)')
-        links = response.css('#imHeader_imMenuObject_04_container > ul > li:nth-child(3) > div.multiple-column > ul > li:nth-child(4) > ul > li:nth-child(2) > ul > li:nth-child(2) > ul a::attr(href)')
+        links = response.css('#imContent a::attr(href)')
         yield from response.follow_all(links, self.parse_item)
 
     def parse_item(self, response):
